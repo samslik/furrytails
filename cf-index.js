2064e6f7675ce51c5615a71f7ef457071600ce01
botchniaque <bochniak.szymon@gmail.com>
2026-05-09 16:24:41 +0200

    fix cloudflare workers

--- cf-index.js
+++ cf-index.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', './views');
+app.use(express.static('./public'));
 
 const renderPage = async (req, res, page) => {
   try {
